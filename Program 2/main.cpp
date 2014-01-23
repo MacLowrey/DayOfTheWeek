a1013f3f5882e2d551254887009934839b6e2fd8
Jon Lowrey <Jon@Jons-MacBook-Pro-3.local>
2014-01-22 19:12:48 -0600

    Initial Commit

---
+++ Program 2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Program 2
+//
+//  Created by Jon Lowrey on 1/22/14.
+//  Copyright (c) 2014 Jon Lowrey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
